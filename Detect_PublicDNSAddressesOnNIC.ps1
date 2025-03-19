bc95ee273d3267456bec55ad4b2229874b24e337
Harris Bowman <harris-bowman@phoenixs.co.uk>
2025-03-19 16:31:59 +0000

    Removed a redundant line of code.

--- Detect_PublicDNSAddressesOnNIC.ps1
+++ Detect_PublicDNSAddressesOnNIC.ps1
@@ -4,10 +4,10 @@
 $interfaces = Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces"
 
 foreach ($interface in $interfaces) {
+    
     # Get the DNS server addresses
     $dnsServers = Get-ItemProperty -Path $interface.PSPath -Name "NameServer" -ErrorAction SilentlyContinue
-    # Get the interface name
-    $dhcpServer = Get-ItemProperty -Path $interface.PSPath -Name "DhcpServer" -ErrorAction SilentlyContinue
+
     if ($dnsServers.NameServer -like "*1.1.*" -or $dnsServers.NameServer -like "*8.8.*" -or $dnsServers.NameServer -like "*192.168.*" -or $dnsServers.NameServer -like "*208.67.*" -or $dnsServers.NameServer -like "*9.9.*" -or $dnsServers.NameServer -like "*149.112.*") {
         Write-Output "Public DNS name servers detected on interface $($interface.PSChildName)"
         Write-Output "DNS Servers: $($dnsServers.NameServer)"
